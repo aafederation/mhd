fc506b8f5cafa92caef6e4bc28114503af37015c
rushilshakya <rushil.shakya@gmail.com>
2020-12-17 11:10:43 -0500

    chore: gulp fonts to static fonts directory

--- design/gulp-tasks/fonts.js
+++ design/gulp-tasks/fonts.js
@@ -5,14 +5,14 @@ const fonts = async () => {
   // Setup of the library instance by setting where we want
   // the output to go. CSS is relative to output font directory
   const instance = new GetGoogleFonts({
-    outputDir: './public/fonts',
-    cssFile: './fonts.css'
-  });
+		outputDir: "../static/fonts",
+		cssFile: "./fonts.css",
+	});
 
   // Grabs fonts and CSS from google and puts in the dist folder
   const result = await instance.download(
-    'https://fonts.googleapis.com/css2?family=Coustard:wght@900&family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap'
-  );
+		"https://fonts.googleapis.com/css?family=Barlow+Condensed:wght@0,400;0,700;0,900;1,400;1,700;1,900|Lora:wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
+	);
 
   return result;
 };
