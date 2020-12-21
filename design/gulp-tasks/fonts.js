a3df79d3805e7506cad2ad12d6e7759fca03c76a
rushilshakya <rushil.shakya@gmail.com>
2020-12-21 18:32:26 -0500

    chore: fonts gulp file download sansita and oswald

--- design/gulp-tasks/fonts.js
+++ design/gulp-tasks/fonts.js
@@ -1,20 +1,27 @@
-const {dest, src} = require('gulp');
-const GetGoogleFonts = require('get-google-fonts');
+const { dest, src } = require("gulp");
+const GetGoogleFonts = require("get-google-fonts");
 
 const fonts = async () => {
-  // Setup of the library instance by setting where we want
-  // the output to go. CSS is relative to output font directory
-  const instance = new GetGoogleFonts({
+	// Setup of the library instance by setting where we want
+	// the output to go. CSS is relative to output font directory
+	const instance = new GetGoogleFonts({
 		outputDir: "../static/fonts",
 		cssFile: "./fonts.css",
 	});
 
-  // Grabs fonts and CSS from google and puts in the dist folder
-  const result = await instance.download(
-		"https://fonts.googleapis.com/css?family=Barlow+Condensed:wght@0,400;0,700;0,900;1,400;1,700;1,900|Lora:wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
+	// Grabs fonts and CSS from google and puts in the dist folder
+
+	//Barlow + Lora
+	//const result = await instance.download(
+	//	"https://fonts.googleapis.com/css?family=Barlow+Condensed:wght@0,400;0,700;0,900;1,400;1,700;1,900|Lora:wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
+	//);
+
+	//Sansita + Oswald
+	const result = await instance.download(
+		"https://fonts.googleapis.com/css?family=Sansita:wght@0,400;0,700;0,900;1,400;1,700;1,900|Oswald:wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
 	);
 
-  return result;
+	return result;
 };
 
 module.exports = fonts;
