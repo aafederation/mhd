cab83003d371aebdca5a6c0a4c0a4aeed48d21f4
rushilshakya <rushil.shakya@gmail.com>
2020-12-24 11:58:32 -0500

    refactor: js script from html to js file

---
+++ static/js/main.js
@@ -0,0 +1,67 @@
+var htfConfig = {
+	//FILTER DECLARATION FOR JAVASCRIPT
+	filters: [
+		{
+			name: "tags",
+			prefix: "tag-",
+			buttonClass: "tag-button",
+			allSelector: "#selectAllTags",
+			attrName: "data-tags",
+			selectedPrefix: "stags-",
+			countPrefix: "ctags-",
+		},
+		{
+			name: "boroughs",
+			prefix: "borough-",
+			buttonClass: "borough-button",
+			allSelector: "#selectAllBoroughs",
+			attrName: "data-boroughs",
+			selectedPrefix: "sboroughs-",
+			countPrefix: "cboroughs-",
+		},
+		{
+			name: "languages",
+			prefix: "language-",
+			buttonClass: "language-button",
+			allSelector: "#selectAllLanguages",
+			attrName: "data-languages",
+			selectedPrefix: "slanguages-",
+			countPrefix: "clanguages-",
+		},
+		{
+			name: "payments",
+			prefix: "payment-",
+			buttonClass: "payment-button",
+			allSelector: "#selectAllPayments",
+			attrName: "data-payments",
+			selectedPrefix: "spayments-",
+			countPrefix: "cpayments-",
+		},
+		{
+			name: "ADAcompliants",
+			prefix: "ADAcompliant-",
+			buttonClass: "ADAcompliant-button",
+			allSelector: "#selectAllADAcompliants",
+			attrName: "data-ADAcompliants",
+			selectedPrefix: "sADAcompliants-",
+			countPrefix: "cADAcompliants-",
+		},
+	],
+	showItemClass: "show-item",
+	filterItemClass: "tf-filter-item",
+	activeButtonClass: "active",
+	counterSelector: "selectedItemCount",
+	populateCount: true,
+	setDisabledButtonClass: "disable-button",
+};
+var htf = new HugoTagsFilter(htfConfig);
+
+function toggleAcc(el) {
+	el.classList.toggle("active");
+	var panel = el.nextElementSibling;
+	if (panel.style.maxHeight) {
+		panel.style.maxHeight = null;
+	} else {
+		panel.style.maxHeight = panel.scrollHeight + "px";
+	}
+}
