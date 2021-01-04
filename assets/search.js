4879bf3da966dfc496223bd4186f38127ca41a90
rushilshakya <rushil.shakya@gmail.com>
2021-01-04 02:29:27 -0500

    feat: fly to coordinate

--- assets/search.js
+++ assets/search.js
@@ -9,8 +9,10 @@
   const filterResults = document.querySelector(idForFilterResults);
   const originalResults = [];
 
-	//mapboxgl accesstoken
+	//mapboxgl related variables
 	mapboxgl.accessToken = "pk.eyJ1IjoiYWJ0cm93IiwiYSI6ImNqejhtbTF2ajE4OTIzbm5samloN2p3MGYifQ.bwAHnbFp6KAZgUmpv3-f8A";
+	let providers;
+	let map;
 
 	//icons to show in the mapbox markers
 	const iconURLs = {
@@ -189,14 +191,19 @@
 
 
 		h6.textContent = myPage.section;
-		a.href = myPage.href;
+		//a.href = myPage.href;
+		a.href = "#";
+		a.id = "link-" + myPage.id;
 		a.textContent = myPage.title;
 		mainDiv.classList.add("show-item");
+		mainDiv.id = "listing-" + myPage.id;
 		mainDiv.setAttribute("data-tag", myPage.tag);
 		mainDiv.setAttribute("data-borough", myPage.borough);
 		mainDiv.setAttribute("data-language", myPage.language);
 		mainDiv.setAttribute("data-payment", myPage.payment);
 		mainDiv.setAttribute("data-adacompliance", myPage.ADAcompliance);
+		const clickFunc = onClickGoToMap(myPage);
+		mainDiv.onclick = clickFunc;
 
 		return clone;
 	}
@@ -222,15 +229,29 @@
    */
 	 function showMap() {
 	 	const aafCoords = [-74.00601627368108, 40.70481604585044];
-		//const providers = getProviders();
+		providers = getProviders();
 
-		const map = new mapboxgl.Map({
+		map = new mapboxgl.Map({
 			container: "map",
 			center: aafCoords, // FullStack coordinates
-			zoom: 10, // starting zoom
+			zoom: 9, // starting zoom
 			style: "mapbox://styles/mapbox/streets-v11", // mapbox has different styles
 		});
 
+    map.on('load', function (e) {
+      /* Add the data to your map as a source */
+			map.addSource('places', {
+				type: "geojson",
+				data: providers
+			})
+
+		  /* For each feature in the GeoJSON object above: */
+		  providers.features.forEach(function(marker) {
+		  	let mark = buildMarker("activities", marker.geometry.coordinates)
+		    mark.addTo(map);
+		  });
+
+    });
 		const marker = buildMarker("activities", aafCoords);
 		marker.addTo(map);
 	}
@@ -252,5 +273,108 @@
     			.setHTML('<h3><a href="/mhd/university-settlement/">University Settlement</a></h3><p>come learn something</p>')
 				);
 	};
+
+  /**
+   * getProviders - returns a json object of all providers
+   * in the format expected by mapbox
+   */
+  function getProviders() {
+    const providers = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              -74.0212758446735,
+              40.63230551173878
+            ]
+          },
+          "properties": {
+	          "id": 0,
+            "phoneFormatted": "(202) 234-7336",
+            "phone": "2022347336",
+            "address": "1471 P St NW",
+            "city": "Arab American Association",
+            "country": "United States",
+            "crossStreet": "at 15th St NW",
+            "postalCode": "20005",
+            "state": "D.C."
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              -73.81661790234168,
+              40.755357177776546
+            ]
+          },
+          "properties": {
+          	"id": 1,
+            "phoneFormatted": "(202) 507-8357",
+            "phone": "2025078357",
+            "address": "2221 I St NW",
+            "city": "Flushing Hospital Medical Center",
+            "country": "United States",
+            "crossStreet": "at 22nd St NW",
+            "postalCode": "20037",
+            "state": "D.C."
+          }
+        }
+      ]
+    };
+    return providers;
+	}
+
+	/**
+	 * fly to the clicked coordinate
+	 */
+	function flyToStore(currentFeature) {
+	  map.flyTo({
+	    center: currentFeature.geometry.coordinates,
+	    zoom: 11
+	  });
+	}
+
+	/**
+	 * create popup that pops up on the map
+	 */
+	function createPopUp(currentFeature) {
+	  var popUps = document.getElementsByClassName('mapboxgl-popup');
+	  /** Check if there is already a popup on the map and if so, remove it */
+	  if (popUps[0]) popUps[0].remove();
+
+	  var popup = new mapboxgl.Popup({ closeOnClick: false })
+	    .setLngLat(currentFeature.geometry.coordinates)
+	    .setHTML('<h3>Service Provider</h3>' +
+	      '<h4>' + currentFeature.properties.address + '</h4>')
+	    .addTo(map);
+	}
+
+	/**
+	 * onclick on the card that flies to the map
+	 */
+	function onClickGoToMap(page) {
+
+		const clickedListing = providers.features[page.id];
+
+		return function() {
+  		flyToStore(clickedListing);
+
+			setTimeout(() => {
+				createPopUp(clickedListing);
+			}, 1000);
+
+		  
+			var activeItem = document.getElementsByClassName('active');
+		  if (activeItem[0]) {
+		    activeItem[0].classList.remove('active');
+		  }
+		  this.classList.add('active');
+		}
+	}
 
 })();
