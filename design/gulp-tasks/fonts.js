8382b57c6f57f57596f85e64618128ff9c2ac28e
rushilshakya <rushil.shakya@gmail.com>
2021-01-05 17:12:53 -0500

    style: change font faces

--- design/gulp-tasks/fonts.js
+++ design/gulp-tasks/fonts.js
@@ -12,14 +12,16 @@ const fonts = async () => {
 	// Grabs fonts and CSS from google and puts in the dist folder
 
 	//Barlow + Lora
-	//const result = await instance.download(
-	//	"https://fonts.googleapis.com/css?family=Barlow+Condensed:wght@0,400;0,700;0,900;1,400;1,700;1,900|Lora:wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
-	//);
+	const result = await instance.download(
+		"https://fonts.googleapis.com/css?family=Barlow+Condensed:wght@0,400;0,700;0,900;1,400;1,700;1,900|Lora:wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
+	);
 
+	/*
 	//Sansita + Oswald
 	const result = await instance.download(
 		"https://fonts.googleapis.com/css?family=Sansita:wght@0,400;0,700;0,900;1,400;1,700;1,900|Oswald:wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
 	);
+	*/
 
 	return result;
 };
