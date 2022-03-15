2b5b6500073fa89e18362c016d270ef5c68f6740
rushilshakya <rushil.shakya@gmail.com>
2022-03-15 12:59:35 +0545

    feat: Complete modal display

--- static/js/modalSignup.js
+++ static/js/modalSignup.js
@@ -1,27 +1,39 @@
 (function () {
-  //1. variables
-  //1.1 local vars
+  //Get from localstorage value for if modal has been displayed
+  let signupModalDisplayed = localStorage.getItem("signupModalDisplayed");
 
-  //1.2 handles to document elements
-  //handle for modal
-  let modal = document.querySelector(".modal");
-  //handle for x-button
-  let xModalClose = document.querySelector(".x-modal-close");
-  //handle for signup button
-  let buttonJoinUs = document.querySelector(".button-join-us");
+  //Only execute rest of code if modal has not been displayed before
+  if (!signupModalDisplayed) {
+    //1. variables
+    //1.1 local vars
 
-  //2. add events
-  //2.1 for x
-  xModalClose.addEventListener("click", function () {
-    modal.style.display = "none";
-  });
-  //2.2 for join button
-  buttonJoinUs.addEventListener("click", function () {
-    modal.style.display = "none";
-  });
+    //1.2 handles to document elements
+    //handle for modal
+    const modal = document.querySelector(".modal");
+    //set property to display modal
+    modal.style.display = "block";
+
+    //handle for x-button
+    const xModalClose = document.querySelector(".x-modal-close");
+    //handle for signup button
+    const buttonJoinUs = document.querySelector(".button-join-us");
+
+    //2. add events
+    //2.1 for x
+    xModalClose.addEventListener("click", function () {
+      closeDisplay(modal);
+    });
+    //2.2 for join button
+    buttonJoinUs.addEventListener("click", function () {
+      closeDisplay(modal);
+    });
+  }
 
   //3. methods
-  function toggleClass(el, className) {
-    el.classList.toggle(className);
+  function closeDisplay(el) {
+    //first hide the modal
+    el.style.display = "none";
+    //then set localstorage for value of signupModalDisplayed to true
+    localStorage.setItem("signupModalDisplayed", true);
   }
 })();
